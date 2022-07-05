a91065098ad4e48e8d9a0ec942e801885c9e02ca
Jan Biehl <jpbiehl@googlemail.com>
2022-07-05 17:42:29 +0200

    introduced new utility methods

--- src/LedUtils.h
+++ src/LedUtils.h
@@ -7,6 +7,17 @@ class LedUtils
 {
 public:
     /**
+     * @brief Get a color from the actual light state
+     * 
+     * @param lightState The state to use to get the color
+     * @return uint32_t A number containing the color information
+     */
+    static uint32_t Color(const LightState* lightState)
+    {
+        return Adafruit_NeoPixel::Color(lightState->red, lightState->green, lightState->blue, lightState->white);
+    }
+
+    /**
      * @brief Get a single value containing every Color, by a color from the color wheel
      * 
      * @param wheelPos The position of the color wheel to use 0-255
@@ -30,6 +41,28 @@ public:
         wheelPos -= 170;
         
         return Adafruit_NeoPixel::Color(wheelPos * 3, 255 - wheelPos * 3, 0);
+    }
+
+    /**
+     * @brief Get the string for a effect
+     * 
+     * @param effect The effect to get the name for
+     * @return const char* The name for the effect
+     */
+    static const char* EffectNameFromEnum(LightEffect effect)
+    {
+        switch (effect)
+        {
+        case LightEffect::solid:
+            return "solid";
+            break;
+        case LightEffect::rainbow:
+            return "rainbow";
+            break;
+        default:
+            return "unknown";
+            break;
+        }
     }
 };
 
